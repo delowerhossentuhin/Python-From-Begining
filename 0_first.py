0bf39c6c93ca34de23ca14a6725e65771c44c360
delowerhossentuhin <dhtuhinmcc@gmail.com>
2025-03-04 02:17:11 +0600

    Printing String, User Input & Type Conversion File Uploaded

---
+++ 0_first.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin=open('input.txt','r')
+sys.stdout=open('output.txt','w')
+
+print("Welcome to Python")
+print('*' * 10)
+name="John"
+age=20
+value=4.5
+print(type(name))
+print(type(value))
+print(int(value * 20.4))
---
+++ 1_user_input_&_Type_Conversion.py
@@ -0,0 +1,16 @@
+import sys
+# sys.stdin=open('input.txt','r')
+# sys.stdout=open('output.txt','w')
+
+name=input("What is Your Name? ")
+print("Hello "+name)
+number=int(input("Give an integer Number: "))
+print(type(number))
+print(f"Your input number is: {number}")
+
+floatVal=float(input("Enter a float Value: "))
+print(f"Type: {type(floatVal)}, Entered float value is: {floatVal}")
+
+birth_Year=input("Enter Your Birth Year: ")
+age=2025-int(birth_Year)
+print(f"Your age is: {age}")
