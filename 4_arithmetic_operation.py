ae462f73d54c8264648047430e8ea96e10f41f63
delowerhossentuhin <dhtuhinmcc@gmail.com>
2025-03-06 02:12:07 +0600

    arithmetic logic file uploades

---
+++ 4_arithmetic_operation.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin=open('input.txt','r')
+sys.stdout=open('output.txt','w')
+
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3) #floating point
+print(10//3) #int 
+print(10**3) # power
+
+x=10
+x=x+3
+print(x)
+x+=3
+print(x)
+x-=3
+print(x)
+x*=3
+print(x)
+x/=3
+print(x)
+x//=3
+print(x)
+
+# Operator Precedence
+# parenthesis
+# exponentiation
+# division or multiplication
+# addition or subtraction
