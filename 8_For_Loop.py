245354e3a0d01414e1a1638206529bbb3b1403dc
delowerhossentuhin <dhtuhinmcc@gmail.com>
2025-03-10 23:53:27 +0600

    control flow and list file upoaded

--- 8_For_Loop.py
+++ 8_For_Loop.py
@@ -1,4 +1,27 @@
-#import sys
-#sys.stdin=open('input.txt','r')
-#sys.stdout=open('output.txt','w')
+import sys
+sys.stdin=open('input.txt','r')
+sys.stdout=open('output.txt','w')
 
+# for printing each item 
+for item in "python":
+    print(item) 
+
+for item in ['sansa','john','arya']:
+    print(item)
+
+#using range function
+for i in range(10):
+    print(i)
+#for a specific range
+for i in range(5,10):
+    print(i)
+
+# range with specific step
+
+for  i in range(1,10,2): #here 1 and 10 is starting and ending point sequentialy also 2 is step size
+    print(i)
+
+#nested loop
+for x in range(1,10,2):
+    for y in range(2,11,2):
+        print(f"x: {x}, y: {y}")
---
+++ 9_Lists_&_2d_List.py
@@ -0,0 +1,49 @@
+import sys
+sys.stdin=open('input.txt','r')
+sys.stdout=open('output.txt','w')
+
+name=["john","sansa","joffery","ned","ramsy","briene"]
+print(name)
+print(name[1])
+print(name[2:5]) # from m'th number to befor n'th nubmer indes value
+name[2]="walter"
+print(name[:4]) # before nth number index value
+print(name[-1]) # nth number from the end
+print(name[-3])
+name[3]=12
+print(name)
+name[3]=name[3]+10
+print(name)
+
+#finding the largest number from a list
+
+numbers=[2,0,15,8,-1,11,9,6]
+max=numbers[0]
+for i in range(len(numbers)):
+    if numbers[i]>=max:
+        max=numbers[i]
+print(max)
+
+# finding the minimum number from a list
+
+min=numbers[0]
+for i in  range(len(numbers)):
+    if numbers[i]<=min:
+        min=numbers[i]
+
+print(min)
+
+matrix=[
+    [1,2,3],
+    [2,4,6],
+    [3,6,9]
+]
+for row in matrix:
+    print(row) # for printing each row
+
+for row in matrix:
+    for item in row:
+        print(item) # for printing each item in matrix
+
+    
+
